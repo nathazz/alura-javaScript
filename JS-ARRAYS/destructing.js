99dba3d015620167e14c6ca86e3ad38c4f1d82fb
Nathan silva <105741461+nathazz@users.noreply.github.com>
2023-06-26 20:32:00 -0300

    Pasta sobre Arrays(lista)

---
+++ JS-ARRAYS/destructing.js
@@ -0,0 +1,12 @@
+const numeroPares = [2,4,6]
+const numeroImpares = [1,3,5]
+
+const numeros = [...numeroPares, ...numeroImpares]
+console.log(numeros)
+
+//... ele joga os dados de um array para outro, fazendo que se "junte"
+
+const num1 = 1
+const num2 = 2
+
+console.log(num1, num2)
---
+++ JS-ARRAYS/lanches.js
@@ -0,0 +1,10 @@
+jantarDeHoje = ['🍔', '🌭', '🍕'] // o pop pega do utlimo elemento pra cima 
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')//puxar, puxando algo
+jantarDeHoje.push('🥼')//puxar algo mias em um array
+console.log(jantarDeHoje)
