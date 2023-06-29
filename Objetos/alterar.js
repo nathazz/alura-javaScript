a90040919f423da7c8232aa97ac2ecb4ff2279f2
Nathan silva <105741461+nathazz@users.noreply.github.com>
2023-06-29 00:48:16 -0300

    Add files via upload
    hahaha

---
+++ Objetos/alterar.js
@@ -0,0 +1,22 @@
+const pessoa = {
+    nome: "Taylane",
+    profissao: "meu amor",
+}
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 12736234";
+
+console.log(pessoa.telefone)
+
+pessoa.nome = "Taylane Rocha"
+console.log(pessoa)
+
+const novaPessoa = {
+    nome: "Pedro"
+
+}
+
+pessoa
