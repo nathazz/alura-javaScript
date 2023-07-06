2871278900b075ec10a586b9a31c44e1c2760262
Nathan silva <105741461+nathazz@users.noreply.github.com>
2023-07-06 19:51:31 -0300

    mais pastas
    conhecendo um pouco mais sobre objetos usando json

---
+++ Objetos/aula5/encontrar.js
@@ -0,0 +1,7 @@
+const clientes = require("./clientes.json");
+
+function encontrar(lista, chave, valor ){
+    return lista.find()
+}
+
+//o find vai retornar um objeto se ele for true(se ele existir)
