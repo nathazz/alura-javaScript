ca97953fba9058accce2ffa6868ee224e5d3f669
Nathan silva <105741461+nathazz@users.noreply.github.com>
2023-07-05 22:20:04 -0300

    Delete Objetos/alura-javaScript/JS-ARRAYS directory

--- Objetos/alura-javaScript/JS-ARRAYS/removendo-notas.js
+++
@@ -1,7 +0,0 @@
-const notas = [ 10,6,8,5.5, 10];
-notas.pop()//remove o ultimo elemento no caso o meu ultimo ali seria o 10
-
-const media = (notas[0] + notas[1]+ notas[2] + notas[3]) / notas.length;
-
-console.log(`A média é ${media}`);
-                     //chamando a const media
--- Objetos/alura-javaScript/JS-ARRAYS/reprovado.js
+++
@@ -1,8 +0,0 @@
-const nomes =["Ana", "Felipe", "Maria", "Mauro"]
-const medias = [7,4.5,8,7.5]
-
-const reprovados = alunos.filter((aluno,idince) =>{
-return medias[indice] < 7;
-})
-
-console.log(reprovados)
